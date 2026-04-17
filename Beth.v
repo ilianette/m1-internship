7b716e10a9a936d79698c9dc75235f1e17651c6c
ilianette <iwoery@mailo.com>
2026-04-17 16:58:16 +0200

    simplifications and doc

--- Beth.v
+++ Beth.v
@@ -48,7 +48,6 @@ Fixpoint bsat {M : BM} (w : worlds) (phi : form) : Type :=
   (* | excl phi psi => exists C w', cov w' C /\ C w /\ acc w' w /\ bsat w' phi /\ ~ bsat w' psi *)
   end.
 
-(* Lemma test {M : BM} ψ ϕ C : (forall w', C w' -> bsat w' ψ + bsat w' ϕ) -> (exists w', C w') -> exists w',  prod (C w') (bsat w' ψ). *)
 
 Lemma mono {M : BM} w w' phi :
   acc w w' -> bsat w phi -> bsat w' phi.
@@ -58,7 +57,7 @@ Proof.
     - intros H1 H2.  eapply cov_ext; try eapply res_mono; eauto.
       (* intuition. apply res_ext in H. firstorder. *)
   - intuition eauto.
-  - intros H (C & H1 & H2). exists (res w' C). split.
+  - intros H [C [H1 H2]]. exists (res w' C). split.
     + eapply res_mono; eauto.
     + intros w1 (w2 & H3 & H4) % res_ext. apply H2 in H3. intuition eauto.
   - clear IHphi1 IHphi2. intros . apply X; try eapply acc_tran; eauto.
@@ -80,70 +79,34 @@ Proof.
     + eapply IHphi1; firstorder eauto.
     + eapply IHphi2; firstorder eauto.
   - intros H1 H2.
-    assert ({D : worlds -> worlds -> Type &
+    assert ( {D : worlds -> worlds -> Type &
                 (forall w', C w' -> cov w' (D w')) *
                 (forall w', C w' -> forall w'0, (D w' w'0) ->
                                      bsat w'0 phi1 + bsat w'0 phi2)
-           }).
+           } ).
     {
-    exists (fun w => match cov_dec C w with
-             | inl proof =>
-                 match (H2 w proof) with
-                 | existT _ Pw (p1 , p2) => Pw
-                 end
-             | inr nCw => fun _ => False
-                    (* | inr nCw => (fun w' Cw => principle-of-explosion (nCw cw)) *)
-             end).
-    split;     intro w';     destruct (cov_dec C w') as [HwC | HnC].
-    * destruct (H2 w' HwC).
-      destruct p as [p1 _]. intros _; apply p1.
-    * intro abs. apply HnC in abs. contradiction.
-    * destruct (H2 w'). destruct p as [_ p2]. intro c . apply p2.
-    * intros; contradiction.
+      exists (fun w => match cov_dec C w with
+               | inl proof => projT1 (H2 w proof)
+               | inr nCw => fun _ => False
+               end).
+
+      split; intro w'; destruct (cov_dec C w') as [HCw' | HnCw']; intro abs; destruct (H2 w') as [Dw' [Hcov Hsat]]; auto.
+      * apply HnCw' in abs; contradiction.
+      * contradiction.
     }
     destruct X as [D [HD1 HD2]].
     eexists.
     split.
-    +
-      eapply cov_union with (C := C) (D:=D).
+    + eapply cov_union.
       intros w' Hw'.
       apply HD1. apply Hw'.
-    + intros w' [wi [H4 H5]]. apply HD2 with (w':=wi). apply H4. apply H5.
-(*       destruct (H2 w' Hw') as [Dw' [HDw' Hϕ]]. *)
-      
-(*       { admit. }  *)
-(*       rewrite <- H. *)
-(*       apply HDw'. *)
-(*     + intros w' H. *)
-(*       destruct H as [wi [ Cwi H3]]. *)
+    + intros w' (wi & H4 & H5).
+      apply HD2 with (w':=wi).
+      apply H4. apply H5.
 
-      
-(*   exact H3. *)
-(*       * destruct (H2 wi Hcov) as [C' [Hcov' Hdec]]; simpl in H3. *)
-(*         unfold D in H3. *)
-(*         cbn in H3. *)
-(*         apply Hdec in H3. *)
-(*       destruct (cov_dec C wi) as [Hcov | Hnocov]. *)
-(*       * *)
-(*       destruct (H2 wi Hcov) as [C' [Hcov' Hdec]]. *)
-(*       simpl in H3. *)
-(*       apply Hdec. *)
-(*       assumption. *)
-(* - exfalso. *)
-(*   apply Hnocov. *)
-(*   exact Cwi. *)
-
- 
-(*     split. *)
-(*     +  *)
-(*       eapply cov_union. intros w' Hw'. admit. (* specialize (H2 w' Hw'). destruct H2 as [Pw' [HPw' Hphi]]. admit. *) *)
-(*     +  *)
-
-(*       specialize (H2 w' H). destruct H2 as [Pw' [HPw' Hphi]]. apply Hphi. admit. *)
-   
   - intros. eapply IHphi2.  apply res_mono with (w0 := w). apply H. apply H0.
     intros v' Hv'.
-    pose proof Hv' as Hv'2.
+    pose proof Hv' as _Hv'.
     apply res_ext in Hv' as (v & Hv &  Hvv').
     eapply X; eauto.
     apply mono with (w:=w').
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 In this branch I put everything that is annoying me in Type, so I can still go forward. It does not mean that everything that lives in Type *needs to* but that I couldn’t immedietaly see how to keep it in Prop.
+
+
+## History
+
+The disjunction case of the past lemma gave trouble because we had no choice principle to grab the covers we needed.
+-> the forcing relation was changed to live in Type
+
+However, this wasn’t enough to build the family of covers needed to apply `cov_union`. We want to build the union-cover of `C`, thus we need to decide which worlds are in `C`. The easy-solution was to add `cov_dec` (as a rule) which decides if a world is in a cover or not. This rule only applies to covers who live in Type (i.e. `C : worlds -> Type`). I tried to minimize the use of Type-covers :
+
+The partial-order still lives in `Prop`, so does the valuation, (partially) the covering relations and the restriction.
+
